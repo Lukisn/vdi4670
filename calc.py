b92ca43e14ca0780c5c0095fbd5d1c2bba98fb05
Lukas Reimer <lukas.reimer@icloud.com>
2018-10-28 22:08:14 +0100

    added calculation script for running calculations with mixtures

---
+++ calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""calc module."""
+
+import mixture
+
+
+def main():
+    """main function."""
+    print("running calculation...")
+    mix = mixture.Mixture()
+    print(mix)
+    print("done.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mixture.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""mixture module."""
+
+
+class Mixture:
+    """Mixture class."""
+
+    def __init__(self):
+        pass
